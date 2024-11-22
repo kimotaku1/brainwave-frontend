eedd73521a0118ac861f0d9f88be8f2465d89d24
Your Name <you@example.com>
2024-11-22 16:44:27 +0545

    code updated

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -4,7 +4,7 @@ import Section from "./Section";
 import Tagline from "./Tagline";
 import { roadmap } from "../constants";
 import { check2, grid, loading1 } from "../assets";
-import { Gradient } from "./design/Roadmap";
+import { Gradient } from "./design/Roadmap.jsx";
 
 const Roadmap = () => (
   <Section className="overflow-hidden" id="roadmap">
